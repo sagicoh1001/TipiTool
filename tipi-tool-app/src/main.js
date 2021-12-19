f4e7c0f8c90d4c9ce9c8cf94358263ab6a881580
urielimelech <urielimelech@hotmail.com>
2021-12-19 18:13:19 +0200

    firestoreDB integration

--- tipi-tool-app/src/main.js
+++ tipi-tool-app/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import writeNewQuestion from './services/addQuestion.js'
+// import { writeSomehing } from './services/dbInit'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// writeNewQuestion('', 'hello?', 'hi!')
